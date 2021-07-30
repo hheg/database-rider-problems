90c93448622b30cfd3fcd3d09535136428dfc651
hheg <henrikhegardtgithub@gmail.com>
2021-07-30 10:46:21 +0200

    Rename test to suffix test so it gets picked up

--- src/test/java/com/example/springboot/NotFindingMethodsTest.java
+++ src/test/java/com/example/springboot/NotFindingMethodsTest.java
@@ -14,7 +14,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class NotFindingMethods {
+public class NotFindingMethodsTest {
 
 	
 	@AfterEach
